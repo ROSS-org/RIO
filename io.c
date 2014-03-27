0e5f153a4fe9084f8d59cb5bd2d9d8f46a32ba8c
Elsa Gonsiorowski (Uranus) <me@gonsie.com>
2014-03-27 11:33:56 -0400

    each mpi rank now only has one metadata struct
    no calloc necessary

--- io.c
+++ io.c
@@ -10,7 +10,7 @@
 
 // Null Initializations
 FILE ** g_io_files;
-io_partition * g_io_partitions;
+io_partition g_io_partition;
 
 // Default Values
 int g_io_number_of_files = 0;
@@ -24,8 +24,8 @@ void io_init(int num_files, int num_partitions) {
     g_io_number_of_files = num_files;
     g_io_number_of_partitions = num_partitions;
 
-    g_io_files = (FILE **) calloc(g_io_number_of_files, sizeof(FILE*));
-    g_io_partitions = (io_partition *) calloc(g_io_number_of_partitions, sizeof(io_partition));
+    //g_io_files = (FILE **) calloc(g_io_number_of_files, sizeof(FILE*));
+    //g_io_partition = (io_partition) calloc(1, sizeof(io_partition));
 
     l_init_flag = 1;
 }
@@ -34,8 +34,8 @@ void io_final() {
     g_io_number_of_files = 0;
     g_io_number_of_partitions = 0;
         
-    free(g_io_files);
-    free(g_io_partitions);
+    //free(g_io_files);
+    //free(g_io_partition);
 	
     l_init_flag = 0;
 }
@@ -57,7 +57,7 @@ void io_read_master_header(char * master_filename) {
 
     int i;
     while (fscanf(master_header, "%d", &i) != EOF) {
-        fscanf(master_header, "%d %d %d %d %d", &(g_io_partitions[i].file), &(g_io_partitions[i].offset), &(g_io_partitions[i].size), &(g_io_partitions[i].data_count), &(g_io_partitions[i].data_size));
+        //fscanf(master_header, "%d %d %d %d %d", &(g_io_partitions[i].file), &(g_io_partitions[i].offset), &(g_io_partitions[i].size), &(g_io_partitions[i].data_count), &(g_io_partitions[i].data_size));
     }
 
     fclose(master_header);
@@ -71,7 +71,7 @@ void io_write_master_header(char * master_filename) {
     fprintf(master_header, "%d %d\n", g_io_number_of_files, g_io_number_of_partitions);
     int i;
     for (i = 0; i < g_io_number_of_partitions; i++) {
-        fprintf(master_header, "%d %d %d %d %d %d\n", i, g_io_partitions[i].file, g_io_partitions[i].offset, g_io_partitions[i].size, g_io_partitions[i].data_count, g_io_partitions[i].data_size);
+        //fprintf(master_header, "%d %d %d %d %d %d\n", i, g_io_partitions[i].file, g_io_partitions[i].offset, g_io_partitions[i].size, g_io_partitions[i].data_count, g_io_partitions[i].data_size);
     }
 
     fclose(master_header);
@@ -81,7 +81,7 @@ void process_metadata(char * data_block, int mpi_rank) {
     int i, offset, count;
     int partition_number;
     for (i = 0; i < g_io_partitions_per_rank; i++) {
-        count = sscanf(data_block, "%d %d %d %d %d %d%n", &partition_number, &g_io_partitions[i].file, &g_io_partitions[i].offset, &g_io_partitions[i].size, &g_io_partitions[i].data_count, &g_io_partitions[i].data_size, &offset);
+        //count = sscanf(data_block, "%d %d %d %d %d %d%n", &partition_number, &g_io_partitions[i].file, &g_io_partitions[i].offset, &g_io_partitions[i].size, &g_io_partitions[i].data_count, &g_io_partitions[i].data_size, &offset);
         assert(count == 6 && "Error: could not read correct number of ints during partition_metadata processing\n");
         assert(partition_number == (mpi_rank * g_io_partitions_per_rank) + i && "Error: an MPI Task is reading the metadata from an unexpected partition\n");
         data_block += offset;
@@ -115,7 +115,7 @@ void io_load_checkpoint(char * master_filename) {
     g_io_partitions_per_rank = g_io_number_of_partitions / number_of_mpitasks;
 
     // Init local partitions
-    g_io_partitions = (io_partition *) calloc(g_io_partitions_per_rank, sizeof(io_partition));
+    //g_io_partition = (io_partition *) calloc(g_io_partitions_per_rank, sizeof(io_partition));
     char * block = (char *) calloc(g_io_partitions_per_rank, partition_md_size);
 
     if (mpi_rank == 0) {
@@ -178,7 +178,7 @@ void io_store_checkpoint(char * master_filename) {
     // close
     fclose(file);
 
-    g_io_partitions[mpi_rank].offset = offset;
+    g_io_partition.offset = offset;
     
     // if not last, send to next writer
     if (write_position != io_ranks_per_file - 1){
@@ -187,11 +187,10 @@ void io_store_checkpoint(char * master_filename) {
     }
 
     // each rank fills in its partition data
-    g_io_partitions[mpi_rank].file = file_number;
-    g_io_partitions[mpi_rank].data_count = g_tw_nlp;
-    if (g_io_partitions[mpi_rank].data_size != 0) {
-        // g_io_partitions[mpi_rank].offset = ?; // get prev space
-        g_io_partitions[mpi_rank].size = g_tw_nlp * g_io_partitions[mpi_rank].data_size;
+    g_io_partition.file = file_number;
+    g_io_partition.data_count = g_tw_nlp;
+    if (g_io_partition.data_size != 0) {
+        g_io_partition.size = g_io_partition.data_count * g_io_partition.data_size;
     } else {
         // non-static data size
         // call model defined function
