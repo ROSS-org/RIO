739275a98d4594825d5974b6a909deefbfbe709b
Elsa Gonsiorowski (Uranus) <me@gonsie.com>
2015-03-24 15:33:52 -0400

    whitespace

--- io-mpi.c
+++ io-mpi.c
@@ -170,7 +170,7 @@ void io_load_checkpoint(char * master_filename) {
     for (i = 0; i < g_io_partitions_on_rank; i++) {
         printf("Rank %d read metadata\n\tpart %d\n\tfile %d\n\toffset %d\n\tsize %d\n\tcount %d\n\n", mpi_rank, my_partitions[i].part, my_partitions[i].file, my_partitions[i].offset, my_partitions[i].size, my_partitions[i].data_count);
     }
-    
+
     // Now data files
     for (i = 1; i < g_io_partitions_on_rank; i++) {
         assert(my_partitions[i].file == my_partitions[0].file && "ERROR: Some rank has partitions spread across files\n");
